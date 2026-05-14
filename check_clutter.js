15fc1742ec049d1a507749dd3160627d814ee1cd
Suraj Yadav <surajyadav200701@gmail.com>
2026-05-14 18:18:13 +0530

    refactor: remove obsolete files and update installation script for GNOME extension

--- check_clutter.js
+++
@@ -1,9 +0,0 @@
-import Clutter from 'gi://Clutter';
-console.log('Clutter loaded:', Clutter);
-console.log('Clutter.DragAction:', Clutter.DragAction);
-try {
-    const action = new Clutter.DragAction();
-    console.log('new Clutter.DragAction() success:', action);
-} catch (e) {
-    console.log('new Clutter.DragAction() error:', e.message);
-}
